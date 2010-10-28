01a00c5e6bf49022832b341c94c1cd4a6c978448
Sven Pfleiderer <sg+github@roothausen.de>
2010-10-28 20:49:53 +0200

    Adding access log for incoming requests

--- mediengewitter.js
+++ mediengewitter.js
@@ -10,15 +10,15 @@
  *
  */
 
-var Sys       = require('sys'),
-    Connect   = require('connect'),
+var Sys = require('sys'),
+    Connect = require('connect'),
     Websocket = require('websocket-server'),
-    Fs        = require('fs'),
-    Log4js    = require('log4js'),
-    Appender  = require('./lib/colorappender.js'),
-    PORT      = 8080,
-    WEBROOT   = __dirname + '/static',
-    LOGFILE   = __dirname + '/log/mediengewitter.log';
+    Fs = require('fs'),
+    Log4js = require('log4js'),
+    Appender = require('./lib/colorappender.js'),
+    PORT = 8080,
+    WEBROOT = __dirname + '/static',
+    LOGFILE = __dirname + '/log/mediengewitter.log';
 
 Log4js.addAppender(Appender.consoleAppender());
 Log4js.addAppender(Log4js.fileAppender(LOGFILE), 'mediengewitter');
@@ -28,22 +28,28 @@ logger.setLevel('DEBUG');
 
 //TODO make options changeable via commandline params
 var IMAGE_PATH = "static/content/",
-    NEW_IMAGES_FILE = IMAGE_PATH + "imageSum",
-    DELAY = 7500;
+NEW_IMAGES_FILE = IMAGE_PATH + "imageSum",
+DELAY = 7500;
+
+var logStream = Fs.createWriteStream(__dirname + '/log/access.log', {
+    encoding: 'utf-8',
+    flags: 'a'
+  });
 
 var httpServer = Connect.createServer(
   Connect.cache(),
   Connect.staticProvider(WEBROOT),
   Connect.gzip(),
-  Connect.errorHandler({ showStack: true })
+  Connect.logger({buffer: true, stream: logStream}),
+  Connect.errorHandler({ showStack: true, dumpExceptions: true})
 );
 
 httpServer.listen(PORT, function () {
-  logger.debug('Webserver successfully started.');
-});
+    logger.debug('Webserver successfully started.');
+  });
 
 var webSocketServer = Websocket.createServer({
-    debug: false,
+    debug: true,
     server: httpServer
   });
 
@@ -63,15 +69,15 @@ function getImageName(currImage, data) {
 var currImage = null; // UNglobalize me
 
 (function doAction() {
-  // TODO refactor me!
-  Fs.readFile(NEW_IMAGES_FILE, "utf8", function (err, data) {
-      if (err) {
-        throw err;
-      }
-      currImage = getImageName(currImage, data.split('\n'));
-      logger.info("Current Image is : " + currImage);
-      toSend = JSON.stringify({'data': 'content/'+ currImage });
-      webSocketServer.broadcast(toSend);
-    });
-  setTimeout(doAction, DELAY);
-}());
+    // TODO refactor me!
+    Fs.readFile(NEW_IMAGES_FILE, "utf8", function (err, data) {
+        if (err) {
+          throw err;
+        }
+        currImage = getImageName(currImage, data.split('\n'));
+        logger.info("Current Image is : " + currImage);
+        var toSend = JSON.stringify({'data': 'content/' + currImage});
+        webSocketServer.broadcast(toSend);
+      });
+    setTimeout(doAction, DELAY);
+  }());
